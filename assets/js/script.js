844a4284a5a28691ab40d1532c4d33c4d10f298e
Kevin <kevin.j.wilkerson@gmail.com>
2021-12-12 12:57:01 -0600

    additional HTML elements added

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+document.getElementById("start-quiz").addEventListener("click", function() {
+    console.log("I was clicked");
+    document.getElementById("site-title").style.display = "none";
+    document.getElementById("question-one").style.display = "block";
+});
+

034a77460272bc05d8334f2f2e0357d3338b8d50
Kevin <kevin.j.wilkerson@gmail.com>
2021-12-12 14:30:00 -0600

    added timer functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
+
+// Start quiz on click
 document.getElementById("start-quiz").addEventListener("click", function() {
     console.log("I was clicked");
     document.getElementById("site-title").style.display = "none";
     document.getElementById("question-one").style.display = "block";
+    
+    // start timer after quiz starts on click
+    var counter = 60;
+    var quizTimer = setInterval(function() {
+        console.log(counter);
+        document.getElementById("timer").innerHTML = counter;
+        counter--;
+        if (counter === 0) {
+            console.log("It's working");
+            clearInterval(quizTimer);
+        }
+    }, 1000);
 });
 
